44e2f4a3b527bbb05b5a6fdc20c302bc5d9bbea7
zvzl <84430227+zvzl@users.noreply.github.com>
2022-06-19 14:04:23 +0000

    更换加速链接

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo -e "======================== 0.1 判断是否安装clash文件 ============
 if [ ! -e '/usr/bin/clash' ]; then
     clash=$(curl -s https://api.github.com/repos/MetaCubeX/Clash.Meta/releases | jq -r .[]."name" | grep -m1 -E "([0-9]{1,2}\.?){3,4}$")
     echo "当前获取clash版本为$clash"
-    if [ $(arch) == aarch64 ]; then     wget -P /usr/bin https://mirror.ghproxy.com/https://github.com/MetaCubeX/Clash.Meta/releases/download/$clash/Clash.Meta-linux-arm64-$clash.gz;     gunzip /usr/bin/Clash.Meta-linux-arm64-$clash.gz;     mv /usr/bin/Clash.Meta-linux-arm64-$clash /usr/bin/clash;     chmod +x /usr/bin/clash; fi
+    if [ $(arch) == aarch64 ]; then     wget -P /usr/bin https://ghproxy.com/https://github.com/MetaCubeX/Clash.Meta/releases/download/$clash/Clash.Meta-linux-arm64-$clash.gz;     gunzip /usr/bin/Clash.Meta-linux-arm64-$clash.gz;     mv /usr/bin/Clash.Meta-linux-arm64-$clash /usr/bin/clash;     chmod +x /usr/bin/clash; fi
     # if [ $(arch) == x86_64 ]; then     wget -P /usr/bin https://mirror.ghproxy.com/https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-amd64-$clash.gz;     gunzip /usr/bin/clash-linux-amd64-$clash.gz;     mv /usr/bin/clash-linux-amd64-$clash /usr/bin/clash;     chmod +x /usr/bin/clash; fi
     
     echo "下载clash完成"
@@ -16,14 +16,14 @@ if [ ! -e '/root/.config/clash/dashboard/index.html' ]; then
     echo "开始移动面板文件到dashboard目录"
     rm -rf /root/.config/clash/dashboard
     mkdir -p /root/.config/clash/dashboard
-    wget https://mirror.ghproxy.com/https://github.com/haishanh/yacd/releases/download/v0.3.5/yacd.tar.xz
+    wget https://ghproxy.com/https://github.com/haishanh/yacd/releases/download/v0.3.5/yacd.tar.xz
     tar -xvf yacd.tar.xz
     mv /public/* /root/.config/clash/dashboard
 fi
 
 if [ ! -e '/root/.config/clash/Country.mmdb' ]; then
     echo "下载Country.mmdb文件"
-    wget -P /root/.config/clash https://mirror.ghproxy.com/https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
+    wget -P /root/.config/clash https://ghproxy.com/https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
 fi
 
 if [ ! -e '/root/.config/clash/iptables.sh' ]; then
@@ -32,8 +32,8 @@ if [ ! -e '/root/.config/clash/iptables.sh' ]; then
 fi
 
 echo "更新geodata文件"
-wget -O /root/.config/clash/geosite.dat https://mirror.ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-wget -O /root/.config/clash/geoip.dat https://mirror.ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -O /root/.config/clash/geosite.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -O /root/.config/clash/geoip.dat https://ghproxy.com/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 
 
 echo -e "======================== 1. 开始自义路由表 ========================\n"
